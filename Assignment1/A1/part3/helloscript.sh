82b82c02070cbea13f2136f9544b183eb022b1d6
Secon01 <73309141+Secon01@users.noreply.github.com>
2026-01-23 16:20:58 +0100

    "initial commit"

---
+++ Assignment1/A1/part3/helloscript.sh
@@ -0,0 +1,4 @@
+echo Hej hej
+echo Mjolk och kaffe
+echo Kaffe eller te?
+echo Brod och kaffe, tack!
---
+++ Labs/01/Lab01_Linux/Task-2/preprocess.s
@@ -0,0 +1,42 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 26, 0	sdk_version 26, 2
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #2                          ; =0x2
+	stur	w8, [x29, #-4]
+	ldur	w0, [x29, #-4]
+	mov	w1, #9                          ; =0x9
+	bl	_add
+	str	w0, [sp, #8]
+	mov	w0, #0                          ; =0x0
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.globl	_add                            ; -- Begin function add
+	.p2align	2
+_add:                                   ; @add
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	w0, [sp, #12]
+	str	w1, [sp, #8]
+	ldr	w8, [sp, #12]
+	ldr	w9, [sp, #8]
+	add	w0, w8, w9
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
+.subsections_via_symbols
